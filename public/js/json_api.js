ktmizugaki/kakeibo

commit d5c5703b60d5f46a5daf110cdf0c436b0a68d178

Hash change support in by-kamoku list

public/js/json_api.js
@@ -105,18 +105,25 @@ var params = (function(_global) {
       query.push([name, value]);
     },
     set: function(name, value) {
-      var initial = true;
+      var initial = true, notify = false;
       query.remove(function(p) {
-        if (initial && p[0] == name) {
-          initial = false;
-          p[1] = value;
+        if (p[0] == name) {
+          if (initial) {
+            initial = false;
+            notify = p[1] == value;
+            p[1] = value;
+            return false;
+          } else {
+            notify = false;
+            return true;
+          }
+        } else {
           return false;
         }
-        return p[0] == name;
       });
       if (initial) {
         query.push([name, value]);
-      } else {
+      } else if (notify) {
         query.notifySubscribers(query());
       }
     },

public/js/manager.js
@@ -1031,19 +1031,41 @@ function ByKamokuManager(tabbar, dialogManager, kamokuManager, date) {
   var self = this;
   self.tabInfo = {label:"科目別",template:"template-bykamoku",data:self};
   self.search = new ByKamoku({
-    date_from: date() || month2str(new Date()),
-    date_to: date() || month2str(new Date()),
+    date_from: params.get("date_from") || date() || month2str(new Date()),
+    date_to: params.get("date_to") || date() || month2str(new Date()),
     kamoku_id: params.get("kamoku_id"),
   });
   self.search.kamokus = kamokuManager.kamokus;
   self.search.kamoku = kamokuManager.computedKamoku(self.search.kamoku_id);
+  params.query.subscribe(function() {
+    var changed = false;
+    var date_from = params.get("date_from");
+    var date_to = params.get("date_to");
+    var id = params.get("kamoku_id");
+    if (date_from && date_from != self.search.date_from()) {
+      self.search.date_from(date_from);
+      changed = true;
+    }
+    if (date_to && date_to != self.search.date_to()) {
+      self.search.date_to(date_to);
+      changed = true;
+    }
+    if (id && id != self.search.kamoku_id()) {
+      self.search.kamoku_id(id);
+      changed = true;
+    }
+    if (changed && tabbar.selected() == 'bykamoku') {
+      self.load(null, true);
+    }
+  });
+
   self.items = ko.observableArray();
   function compareItemByDate(a,b) {
     if (a.date() < b.date()) return -1;
     if (a.date() > b.date()) return 1;
     return a.id() - b.id();
   }
-  self.load = function() {
+  self.load = function(form, keep_query) {
     var q = self.search.toParams();
     var errors = null;
     if (!is_month_str(q.date_from)) {
@@ -1063,7 +1085,11 @@ function ByKamokuManager(tabbar, dialogManager, kamokuManager, date) {
       return;
     }
     self.items([]);
-    params.set("kamoku_id", q.kamoku_id);
+    if (keep_query !== true) {
+      params.set("kamoku_id", q.kamoku_id);
+      params.set("date_from", q.date_from);
+      params.set("date_to", q.date_to);
+    }
     return Promise.all([Item.search(q)]).then(function(res){
       var items = res[0]();
       var sum = 0;

public/js/page.js
@@ -4,7 +4,7 @@ var page = (function(_global) {
   var dialogManager = koDialogManager({});
   var date = ko.observable(params.get("date") || month2str(new Date()));
   date.subscribe(function(date) { params.set("date", date); });
-  params.query.subscribe(function() { var d = params.get("date"); if (d) date(d); });
+  params.query.extend({rateLimit:10}).subscribe(function() { var d = params.get("date"); if (d) date(d); });
   var kamokus = new KamokuManager(tab, dialogManager);
   var tmpls = new TmplManager(tab, dialogManager, kamokus);
   var lists = new ListManager(tab, dialogManager, kamokus, tmpls, date);

commit 078889440ac27ac8d947497b7246be2cdb2dc8fe

Fix updating path params
Broken by d5c5703b60d5f46a5daf110cdf0c436b0a68d178.

public/js/json_api.js
@@ -110,7 +110,7 @@ var params = (function(_global) {
         if (p[0] == name) {
           if (initial) {
             initial = false;
-            notify = p[1] == value;
+            notify = p[1] != value;
             p[1] = value;
             return false;
           } else {
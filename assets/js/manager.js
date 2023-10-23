10dc3948ad314e3289c3c37d44346496b5c53f95
teru <ktmizugaki@gmail.com>
2023-10-23 20:41:53 +0900

    Separate dialog from KamokuManager

--- assets/js/manager.js
+++ assets/js/manager.js
@@ -1,57 +1,68 @@
-function KamokuManager(tabbar, dialogManager, dataStore) {
-  var self = this;
-  self.tabInfo = {label:"勘定科目",template:"template-kamokus",data:self};
-  self.categories = dataStore.categories;
-  self.kamokus = dataStore.kamokus;
-  var dialog = self.dialog = {
-    template:"template-kamoku-form",
-    id: "kamoku-dialog",
-    parent: self,
+function KamokuDialog(dialogManager, dataStore) {
+  this.template = "template-kamoku-form";
+  this.id = "kamoku-dialog";
+  this.title = ko.pureComputed(this.title.bind(this));
+  this.data = {
     dataStore: dataStore,
     value: ko.observable(null),
+    onSave: this.close.bind(this),
+    onDel: this.close.bind(this),
   };
-  dialog.data = dialog;
-  dialog.title = ko.pureComputed(function() {
-    var value = dialog.value();
-    return value? value.id()? "勘定科目の更新": "勘定科目の作成": null;
-  });
-  dialog.onDialogOpen = function(handle, element) {
-    element.querySelector("select, input").focus();
-  };
-  dialog.onDialogClose = function(handle) {
-    if (dialog.handle != handle) {
-      return;
-    }
-    dialog.value(null);
-    dialog.handle = null;
-  };
-  self.loadAll = function() {
+  this.handle = null;
+  this.dialogManager = dialogManager;
+  this.dataStore = dataStore;
+  this.onDialogOpen = this.onDialogOpen.bind(this);
+  this.onDialogClose = this.onDialogClose.bind(this);
+}
+KamokuDialog.prototype.title = function() {
+  var value = this.data.value();
+  return value? value.id()? "勘定科目の更新": "勘定科目の作成": null;
+};
+KamokuDialog.prototype.open = function(kamoku) {
+  var handle = this.dialogManager.open(this);
+  if (!handle) {
+    return false;
+  }
+  this.handle = handle;
+  var value = new Kamoku(kamoku&&kamoku.toObject());
+  value.category = this.dataStore.computedCategory(value.category_id);
+  this.data.value(value);
+  return true;
+};
+KamokuDialog.prototype.close = function() {
+  if (this.handle) {
+    this.dialogManager.close(this.handle);
+  }
+};
+KamokuDialog.prototype.onDialogOpen = function(handle, element) {
+  element.querySelector("select, input").focus();
+};
+KamokuDialog.prototype.onDialogClose = function(handle) {
+  if (this.handle != handle) {
+    return;
+  }
+  this.data.value(null);
+  this.handle = null;
+};
+
+function KamokuManager(tabbar, dialogManager, dataStore) {
+  this.tabInfo = {label:"勘定科目",template:"template-kamokus",data:this};
+  this.categories = dataStore.categories;
+  this.kamokus = dataStore.kamokus;
+  this.loadAll = function() {
     return Promise.all([Category.all(), Kamoku.all()]).then(function(res){
       dataStore.setCategories(res[0]());
       dataStore.setKamokus(res[1]());
     });
   };
 
-  function openKamoku(kamoku) {
-    var handle = dialogManager.open(dialog);
-    if (!handle) {
-      return false;
-    }
-    dialog.handle = handle;
-    var value = new Kamoku(kamoku&&kamoku.toObject());
-    value.category = dataStore.computedCategory(value.category_id);
-    dialog.value(value);
-    return true;
-  }
-  function closeKamoku() {
-    if (dialog.handle) {
-      dialogManager.close(dialog.handle);
-    }
-  }
-  dialog.addKamoku = function() { return openKamoku(); };
-  dialog.editKamoku = function() { return openKamoku(this); };
-  dialog.onSave = closeKamoku;
-  dialog.onDel = closeKamoku;
+  var dialog = new KamokuDialog(dialogManager, dataStore);
+  this.addKamoku = function() {
+    return dialog.open();
+  };
+  this.editKamoku = function(kamoku) {
+    return dialog.open(kamoku);
+  };
 }
 
 function TmplManager(tabbar, dialogManager, dataStore) {

245d13463dc9985523e83cb1adcc44259f6ae1de
teru <ktmizugaki@gmail.com>
2023-10-25 19:09:25 +0900

    Separate tmpls page to its own file

--- assets/js/page.js
+++ assets/js/page.js
@@ -9,7 +9,7 @@ var page = (function(_global) {
   date.subscribe(function(date) { params.set("date", date); });
   params.query.extend({rateLimit:10}).subscribe(function() { var d = params.get("date"); if (d) date(d); });
   var kamokus = new KamokusPage(tab, dialogManager, dataStore);
-  var tmpls = new TmplManager(tab, dialogManager, dataStore);
+  var tmpls = new TmplsPage(tab, dialogManager, dataStore);
   var lists = new ListManager(tab, dialogManager, dataStore, date);
   var summary = new SummaryManager(tab, dialogManager, dataStore, date);
   var bykamoku = new ByKamokuManager(tab, dialogManager, dataStore, date);
